f6499da801aaae5f612ce952b822dc337ec371ce
dbalz3 <dbalazster@gmail.com>
2016-06-23 20:52:56 -0500

    re-initialize SuperSteam Firefox repo

---
+++ data/content_on_start.js
@@ -0,0 +1,29 @@
+//This file prevents HighlightPlayer from first initialisation by using our own stub file instead.
+console.log ("Loading updated Highlight player Stub");
+var s = document.createElement('script');
+s.src = s.src = self.options.highlight_player_stub;
+s.onload = function() {
+	this.parentNode.removeChild(this);
+};
+(document.head || document.documentElement).appendChild(s);
+
+/*function loadHighlightPlayerStub() {
+	var addedHighlightPlayerStub = false;
+	return {
+		addScript:function(){
+			console.log ("loading request");
+			if(!addedHighlightPlayerStub){
+				console.log ("load script");
+				var s = document.createElement('script');
+				s.src = s.src = self.options.highlight_player_stub;
+				s.onload = function() {
+					this.parentNode.removeChild(this);
+				};
+				(document.head || document.documentElement).appendChild(s);
+				addedHighlightPlayerStub = true;
+			}
+		}
+	}
+}*/
+//var highlightplayer = loadHighlightPlayerStub();
+//highlightplayer.addScript();
---
+++ data/player_init.js
@@ -0,0 +1,47 @@
+console.log("PLAYER INIT INIT");
+//Initialization of our HighlightPlayer class
+var rgYoutubeURLs = rgYoutubeURLs || new Array();
+
+var player;
+
+player = new HighlightPlayer( {
+	elemPlayerArea: 'highlight_player_area',
+	elemStrip: 'highlight_strip',
+	elemStripScroll: 'highlight_strip_scroll',
+	elemSlider: 'highlight_slider',
+	rgMovieFlashvars: rgMovieFlashvars,
+	rgScreenshotURLs: rgScreenshotURLs,
+	rgYoutubeURLs: rgYoutubeURLs,
+	rgDefaultMovieFlashvars: rgCommonFlashVars,
+	bUseHTMLPlayer: bShouldUseHTML5
+} );
+
+$J('#highlight_slider_right').attr('onclick','').unbind('click');
+$J('#highlight_slider_left').attr('onclick','').unbind('click');
+
+$J('#highlight_slider_right').click( function() {
+	player.Transition( true );
+});
+$J('#highlight_slider_left').click( function() {
+	player.TransitionBack( true );
+});
+
+//TODO: check possible bugs on double init
+BindStoreTooltip( $J('[data-store-tooltip]') )
+
+if( window.location.hash )
+{
+	var ssid = window.location.hash.substr(1);
+	player.HighlightScreenshot(ssid);
+}
+
+if( bShouldUseHTML5 )
+{
+	for(key in rgMovieFlashvars)
+	{
+		if( key.indexOf("movie") != -1)
+		{
+			jQuery("#"+key).videoControls();
+		}
+	}
+}
